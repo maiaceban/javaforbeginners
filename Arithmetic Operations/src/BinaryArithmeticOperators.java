9044dfd56bb761a1375fddd28d7530306e64ab45
Ceban Maia <maya.ceb.96@gmail.com>
2022-10-16 11:24:00 +0300

    Java for beginners

---
+++ Arithmetic Operations/src/BinaryArithmeticOperators.java
@@ -0,0 +1,23 @@
+public class BinaryArithmeticOperators {
+    public static void main(String[] args) {
+
+        //addition +
+        System.out.println(78 + 78);
+
+        //subtraction -
+        System.out.println(36 - 33);
+
+        //multiplication *
+        System.out.println(36 * 36);
+
+        // integer division /
+        System.out.println(78 / 73);
+
+        // remainder %
+        System.out.println(23 % 7);
+
+        // more complex expressions:
+        System.out.println(7 + 6 * 8 - 3);
+        System.out.println((7 + 6) * (8 - 3));
+    }
+}
---
+++ Arithmetic Operations/src/FibonacciSeries.java
@@ -0,0 +1,20 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int limit = 10;
+        long[] series = new long[limit];
+
+        series[0] = 0;
+        series[1] = 1;
+
+        for (int i = 2; i < limit; i++) {
+            series[i] = series[i - 1] + series[i - 1];
+
+        }
+        System.out.println("Fibonacci series--->" + limit);
+        for (int i = 0; i < limit; i++) {
+            System.out.print(series[i] + " ");
+
+        }
+
+    }
+}
