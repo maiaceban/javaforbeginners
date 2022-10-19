53554d0c1bc52ae19cf9d21f09fa023239a20043
Ceban Maia <maya.ceb.96@gmail.com>
2022-10-19 18:38:37 +0300

    Bank account

---
+++ example2/src/CheckingAccount.java
@@ -0,0 +1,8 @@
+public class CheckingAccount extends BankAccount{
+    double fee;
+
+    public CheckingAccount(String number, Long balance, double fee) {
+        super(number, balance);
+        this.fee = fee;
+    }
+}
---
+++ example2/src/SavingAccount.java
@@ -0,0 +1,8 @@
+public class SavingAccount extends BankAccount {
+    double interestRate;
+
+    public SavingAccount(String number, Long balance, double interestRate) {
+        super(number, balance);
+        this.interestRate = interestRate;
+    }
+}
